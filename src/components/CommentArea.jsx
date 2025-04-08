d198164a7805cd40be409595d66a95f54d806b7e
MaddalenaRen <ileniarennella@gmail.com>
2025-04-08 16:23:48 +0200

    commit

--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -1,15 +1,15 @@
-import { Component } from 'react'
-import CommentList from './CommentList'
-import AddComment from './AddComment'
-import Loading from './Loading'
-import Error from './Error'
+import { Component } from "react";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
+import Loading from "./Loading";
+import Error from "./Error";
 
 class CommentArea extends Component {
   state = {
     comments: [],
     isLoading: false,
     isError: false,
-  }
+  };
 
   // componentDidMount = async () => {
   //   try {
@@ -41,34 +41,35 @@ class CommentArea extends Component {
     if (prevProps.asin !== this.props.asin) {
       this.setState({
         isLoading: true,
-      })
+      });
       try {
         let response = await fetch(
-          'https://striveschool-api.herokuapp.com/api/comments/' +
+          "https://striveschool-api.herokuapp.com/api/comments/" +
             this.props.asin,
           {
             headers: {
-              Authorization: 'Bearer inserisci-qui-il-tuo-token',
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2Y1MTY3ODgxYjBkZDAwMTUwYTdhN2EiLCJpYXQiOjE3NDQxMTUzMjAsImV4cCI6MTc0NTMyNDkyMH0.t2jjf0t-OtsO82jjbj54deP3kO3WJuXh3MPI-VF7QSE",
             },
           }
-        )
-        console.log(response)
+        );
+        console.log(response);
         if (response.ok) {
-          let comments = await response.json()
+          let comments = await response.json();
           this.setState({
             comments: comments,
             isLoading: false,
             isError: false,
-          })
+          });
         } else {
-          this.setState({ isLoading: false, isError: true })
+          this.setState({ isLoading: false, isError: true });
         }
       } catch (error) {
-        console.log(error)
-        this.setState({ isLoading: false, isError: true })
+        console.log(error);
+        this.setState({ isLoading: false, isError: true });
       }
     }
-  }
+  };
 
   render() {
     return (
@@ -78,8 +79,8 @@ class CommentArea extends Component {
         <AddComment asin={this.props.asin} />
         <CommentList commentsToShow={this.state.comments} />
       </div>
-    )
+    );
   }
 }
 
-export default CommentArea
+export default CommentArea;
